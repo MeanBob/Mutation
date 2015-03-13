8abe8f7fa2cc651ca6cfd6aafe3d2b3283003a2c
Mark <mark.r.breeden@gmail.com>
2015-03-12 22:25:04 -0600

    UI fixed to work with multiple resolutions.  Avatar can now rotate

--- Mutation/Assets/Scripts/InputHandler.cs
+++ Mutation/Assets/Scripts/InputHandler.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class InputHandler : MonoBehaviour {
-
+    GameObject avatar;
+    float rotationSpeed = 1;
 	// Use this for initialization
 	void Start () {
-	
+        avatar = GameObject.Find("Avatar");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.touchCount > 0)
+        {
+            for(int i = 0; i < Input.touchCount; i++)
+            {
+                Touch currentTouch = Input.GetTouch(i);
+                Debug.Log("Touch Delta: " + currentTouch.deltaPosition);
+                switch (currentTouch.phase)
+                {
+                    case TouchPhase.Moved:
+                        avatar.transform.Rotate(currentTouch.deltaPosition.y * rotationSpeed, -currentTouch.deltaPosition.x * rotationSpeed, 0, Space.World);
+                        break;
+                }
+            }
+        }
 	
 	}
 }
