d324387a8610e9467412d868d817e25bdd324a4e
johnschwarz <johnschwarzorz@gmail.com>
2015-03-15 22:18:31 -0600

    Added Bear and Wolf Mutation and Monster scripts.
    but it's broken!

---
+++ Mutation/Assets/Scripts/Monsters/BearMonster.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearMonster : MonoBehaviour {
+
+	// Use this for initialization
+	public override void Init()
+	{
+		monsterName = "Bear";
+		strength = 40;
+		speed = 10;
+		intelligence = 5;
+		
+		headMinDamage = 1;
+		headMaxDamage = 8;
+		armMinDamage = 1;
+		armMaxDamage = 10;
+		legMinDamage = 1;
+		legMaxDamage = 8;
+		
+		bonusDamage = strength;
+		
+		base.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Mutation/Assets/Scripts/Monsters/WolfMonster.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfMonster : MonoBehaviour {
+
+	// Use this for initialization
+	public override void Init()
+	{
+		monsterName = "Wolf";
+		strength = 10;
+		speed = 15;
+		intelligence = 5;
+		
+		headMinDamage = 1;
+		headMaxDamage = 8;
+		armMinDamage = 1;
+		armMaxDamage = 6;
+		legMinDamage = 1;
+		legMaxDamage = 6;
+		
+		bonusDamage = strength;
+		
+		base.Init();
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
