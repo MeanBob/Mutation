59f4de3c336a537cabfc7895e94d22ac4fd8325c
johnschwarz <johnschwarzorz@gmail.com>
2015-03-15 22:11:56 -0600

    Test2

---
+++ Mutation/Assets/Scripts/Mutations/BearMutation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearMutation : MonoBehaviour {
+
+	// Use this for initialization
+	public override void Init() {
+		mutationName = "Bear";
+		headMinDamage = 1;
+		headMaxDamage = 8;
+		armMinDamage = 1;
+		armMaxDamage = 10;
+		legMinDamage = 1;
+		legMaxDamage = 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Mutation/Assets/Scripts/Mutations/WolfMutation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfMutation : MonoBehaviour {
+
+	// Use this for initialization
+	public override void Init() {
+		mutationName = "Wolf";
+		headMinDamage = 1;
+		headMaxDamage = 8;
+		armMinDamage = 1;
+		armMaxDamage = 6;
+		legMinDamage = 1;
+		legMaxDamage = 6;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
