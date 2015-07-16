aac6e5a6a2f18dfbaf349d61a697a120073ade57
johnschwarz <johnschwarzorz@gmail.com>
2015-07-15 23:54:13 -0600

    SoundTests

---
+++ Mutation/Assets/Scripts/SoundControler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundControler : MonoBehaviour {
+	//[RequireComponent(typeof(AudioSource))]
+	public AudioClip drum1;
+	public AudioClip drum2;
+	public AudioClip drum3;
+	public AudioClip flute1;
+	public AudioClip flute2;
+	public AudioClip flute3;
+	public AudioClip flute4;
+
+
+	public AudioClip singingLoop;
+	AudioSource audioPlayer;
+
+	// Use this for initialization
+	void Start () {
+		audioPlayer = GetComponent<AudioSource>();
+		PlayVoice();
+
+	}
+
+	public void PlayFlute2()
+	{
+		audioPlayer.PlayOneShot(flute2, 0.4f);
+	}
+	public void PlayFlute3()
+	{
+		audioPlayer.PlayOneShot(flute3, 0.4f);
+	}
+	public void PlayFlute4()
+	{
+		audioPlayer.PlayOneShot(flute4, 0.4f);
+	}
+
+
+
+
+	public void PlayFlute1()
+	{
+		audioPlayer.PlayOneShot(flute1, 0.4f);
+	}
+
+	public void PlayDrum3()
+	{
+		audioPlayer.PlayOneShot(drum3);
+	}
+
+	public void PlayDrum2()
+	{
+		audioPlayer.PlayOneShot(drum2);
+	}
+
+	public void PlayDrum1()
+	{
+		audioPlayer.PlayOneShot(drum1);
+	}
+	public void PlayVoice()
+	{
+		audioPlayer.PlayOneShot(singingLoop, 0.4f);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
